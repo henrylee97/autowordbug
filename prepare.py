537d7e671b025e29e3f1dae28bbf6c99d2eee01e
Henry Lee <gbvrcx@gmail.com>
2020-12-14 04:35:23 +0900

    refactor: add more description on help

--- prepare.py
+++ prepare.py
@@ -6,7 +6,7 @@ from autowordbug.dataloader import create_dataset
 
 def main(argv):
   parser = argparse.ArgumentParser('Data preparation script')
-  parser.add_argument('-d', '--data', metavar='<dir>', type=Path, default=Path('data/ag_news/WordRNN'), help='Folder that contains csv files')
+  parser.add_argument('-d', '--data', metavar='<dir>', type=Path, default=Path('data/ag_news/WordRNN'), help='Folder that contains csv files (default: data/ag_news/WordRNN)')
   parser.add_argument('--train', metavar='<float>', type=float, default=0.8, help='Portion of train set (default: 0.8)')
   parser.add_argument('--val', metavar='<float>', type=float, default=0.1, help='Portion of validation set (default: 0.1)')
   parser.add_argument('--test', metavar='<float>', type=float, default=0.1, help='Portion of test set (default: 0.1)')
